c521a6bb35efe6f547c3acda2d118eaa2fe6c988
Auro Florentino <auro@merco.net>
2021-01-30 15:27:29 -0300

    1: Welcome to the machine!

--- gmachine.go
+++ gmachine.go
@@ -4,3 +4,18 @@ package gmachine
 // DefaultMemSize is the number of 64-bit words of memory which will be
 // allocated to a new G-machine by default.
 const DefaultMemSize = 1024
+
+// MachineStruct is a struct type defined wich represents P register and memory allocation
+type MachineStruct struct {
+	P      uint64
+	Memory []uint64
+}
+
+// New returns a struct with predefined values
+func New() *MachineStruct {
+	memSlice := make([]uint64, 1024)
+	for i := range memSlice {
+		memSlice[i] = uint64(0)
+	}
+	return &MachineStruct{0, memSlice}
+}
